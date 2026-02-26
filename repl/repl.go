8fb31d912ef15881a59d4ebaae3d59a445f5e1ac
the-1aw <bguilliod@gmail.com>
2026-02-26 14:55:52 +0100

    feat: evolve monkey business from read lex print loop to read parse print loop

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,31 @@ import (
 	"io"
 
 	"github.com/the-1aw/monkey-business/lexer"
-	"github.com/the-1aw/monkey-business/token"
+	"github.com/the-1aw/monkey-business/parser"
 )
 
 const PROMPT = ">> "
+const MONKEY_FACE = `	    __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, error []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Whoops! We ran into some mokey business here!\n")
+	io.WriteString(out, "Parser errors:\n")
+	for _, msg := range error {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -21,9 +42,15 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
 }
