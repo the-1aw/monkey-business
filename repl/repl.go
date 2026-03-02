59b5634e25438c3be07fa09e9391ef7183a65684
the-1aw <bguilliod@gmail.com>
2026-03-02 14:53:53 +0100

    feat(repl): use the evaluator to implement the eval step of the repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/the-1aw/monkey-business/evaluator"
 	"github.com/the-1aw/monkey-business/lexer"
 	"github.com/the-1aw/monkey-business/parser"
 )
@@ -43,14 +44,17 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.New(line)
 		p := parser.New(l)
-		program := p.ParseProgram()
 
+		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
