56458da49fd6ac639843cd733d437ef4f9325eff
the-1aw <bguilliod@gmail.com>
2026-03-06 17:12:50 +0100

    feat(parser): add string support

--- ast/ast.go
+++ ast/ast.go
@@ -268,3 +268,16 @@ func (ie *InfixExpression) String() string {
 	out.WriteString(")")
 	return out.String()
 }
+
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode() {}
+func (sl *StringLiteral) TokenLiteral() string {
+	return sl.Token.Literal
+}
+func (sl *StringLiteral) String() string {
+	return sl.Token.Literal
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -504,6 +504,24 @@ func TestCallExpressionArgumentParsing(t *testing.T) {
 	}
 }
 
+func TestStingLiteralExpression(t *testing.T) {
+	input := `"hello world";`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+	literal, ok := stmt.Expression.(*ast.StringLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.StringLiteral. got=%T", stmt.Expression)
+	}
+	if literal.Value != "hello world" {
+		t.Errorf("literal.Value not %q, got=%q", "hello world", literal.Value)
+	}
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLIteral not 'let'. got=%q", s.TokenLiteral())
