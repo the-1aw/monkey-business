f2006d0d23b14b4793b6761ccef8086afb106919
the-1aw <bguilliod@gmail.com>
2026-03-23 15:50:38 +0100

    feat(parser): add support for hash listerals

--- ast/ast.go
+++ ast/ast.go
@@ -327,3 +327,25 @@ func (ie *IndexExpression) String() string {
 
 	return out.String()
 }
+
+type HashLiteral struct {
+	Token token.Token
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode() {}
+func (hl *HashLiteral) TokenLiteral() string {
+	return hl.Token.Literal
+}
+func (hl *HashLiteral) String() string {
+	out := bytes.Buffer{}
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+" : "+value.String())
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+	return out.String()
+}
