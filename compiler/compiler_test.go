19e2c4891581509df29a93cb90ecb55994ca0f14
the-1aw <bguilliod@gmail.com>
2026-04-22 15:46:50 +0200

    feat(compiler): generate bytecode for array literal

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -17,6 +17,47 @@ type compilerTestCase struct {
 	expectedInstructions []code.Instructions
 }
 
+func TestArrayLIterals(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "[]",
+			expectedConstants: []any{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpArray, 0),
+				code.Make(code.OpPop),
+			},
+		}, {
+			input:             "[1, 2, 3]",
+			expectedConstants: []any{1, 2, 3},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpArray, 3),
+				code.Make(code.OpPop),
+			},
+		}, {
+			input:             "[1 + 2, 3 - 4, 5 * 6]",
+			expectedConstants: []any{1, 2, 3, 4, 5, 6},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpAdd),
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpConstant, 3),
+				code.Make(code.OpSub),
+				code.Make(code.OpConstant, 4),
+				code.Make(code.OpConstant, 5),
+				code.Make(code.OpMul),
+				code.Make(code.OpArray, 3),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
 func TestStringExpression(t *testing.T) {
 	tests := []compilerTestCase{
 		{
