468ac2c654def5cb0ed5b55c01d835daef446fa1
the-1aw <bguilliod@gmail.com>
2026-04-23 14:28:33 +0200

    feat(compiler): generate bytecode for value indexing

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -191,6 +191,16 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 		c.emit(code.OpHash, len(node.Pairs)*2)
+	case *ast.IndexExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+		err = c.Compile(node.Index)
+		if err != nil {
+			return err
+		}
+		c.emit(code.OpIndex)
 	case *ast.StringLiteral:
 		str := &object.String{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(str))
