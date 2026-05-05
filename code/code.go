ddb0440bd89a2204b293be6886c51093434d90b6
the-1aw <bguilliod@gmail.com>
2026-05-05 17:09:48 +0200

    feat(bytecode): add new opcode for local bindings and support for 1 byte operands

--- code/code.go
+++ code/code.go
@@ -71,6 +71,8 @@ const (
 	OpCall
 	OpReturn
 	OpReturnValue
+	OpGetLocal
+	OpSetLocal
 )
 
 // This defines the metadata of an opcode.
@@ -106,6 +108,11 @@ var definitions = map[Opcode]*Definition{
 	OpCall:          {"OpCall", []int{}},
 	OpReturn:        {"OpReturn", []int{}},
 	OpReturnValue:   {"OpReturnValue", []int{}},
+	// NOTE: We could have used 2 bytes operand here but choose one for two reason
+	// 1) 256 local bindings should be more than enough for one scope so might as well save some memory
+	// 2) We used 2 bytes for every operand so far and I want to see something different ^^'
+	OpGetLocal: {"OpGetLocal", []int{1}},
+	OpSetLocal: {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -132,6 +139,8 @@ func Make(op Opcode, operands ...int) []byte {
 	for i, operand := range operands {
 		width := def.OperandWidths[i]
 		switch width {
+		case 1:
+			instruction[offset] = byte(operand)
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(operand))
 		}
@@ -148,11 +157,17 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 		offset += width
 	}
 
 	return operands, offset
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func ReadUint16(ins Instructions) uint16 {
